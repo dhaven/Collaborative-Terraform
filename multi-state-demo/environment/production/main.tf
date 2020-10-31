54ebb8f66616d208cebe2a52f8b9b1ee1d936317
David Haven <david.haven@gonitro.com>
2020-10-31 14:58:01 +0000

    test create s3 bucket (should fail from lack of permissions)

--- multi-state-demo/environment/production/main.tf
+++ multi-state-demo/environment/production/main.tf
@@ -2,27 +2,12 @@ resource "null_resource" "my_prod_resource" {
 
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = [aws_vpc.main.cidr_block]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl    = "private"
 
   tags = {
-    Name = "allow_tls"
+    Name        = "My bucket"
+    Environment = "Dev"
   }
 }
